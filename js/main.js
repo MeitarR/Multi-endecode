9f80b6cd017e98fe9b8c4ef2c30758f5b2a649b6
Meitar Reihan <meitarr013@gmail.com>
2018-02-17 15:33:48 +0200

    implemented the new design, made the html generetion more generic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,52 @@
+function generateDiv(name, body, color) {
+    body = typeof body !== 'undefined' ? body : generateDefaultDivBody(name);
+    color = typeof color !== 'undefined' ? color : generateRandomMaterialColor();
+    return (
+        "    <div class=\"card\" style=\"background-color: " + color +"\">\n" +
+        "        <h3>{name}</h3>\n" +
+        "        <textarea id='{name}Text' class='materialize-textarea'></textarea>\n" +
+        "        <div class=\"bottom-content\">\n" +
+            body +
+        "        </div>\n" +
+        "    </div>"
+    ).replace(/{name}/g, name)
+}
+
+function generateDefaultDivBody(name) {
+    return (
+        "<button class='waves-effect waves-light btn myBtn' onclick='{name}Obj.doEncrypt()'>encrypt</button>\n" +
+        "<button class='waves-effect waves-light btn myBtn' onclick='{name}Obj.doDecrypt()'>decrypt</button>\n"
+    ).replace(/{name}/g, name)
+}
+
+function generateRandomMaterialColor() {
+    var colors = [
+        "#ef9a9a", "#e57373",
+        "#f48fb1", "#f06292",
+        "#ce93d8", "#ba68c8",
+        "#b39ddb", "#9575cd",
+        "#9fa8da", "#7986cb",
+        "#90caf9", "#64b5f6",
+        "#81d4fa", "#4fc3f7",
+        "#80deea", "#4dd0e1",
+        "#80cbc4", "#4db6ac",
+        "#a5d6a7", "#81c784",
+        "#c5e1a5", "#aed581",
+        "#e6ee9c", "#dce775",
+        "#fff59d", "#fff176",
+        "#ffe082", "#ffd54f",
+        "#ffcc80", "#ffb74d",
+        "#ffab91", "#ff8a65",
+        "#bcaaa4", "#a1887f"
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
 function PlainText(name) {
     this.name = name;
 
     this.createHTML = function () {
-        $(document.body).append((
-            "<div>" +
-            "   <h1>{name}</h1>" +
-            "   <textarea id='{name}Text'></textarea>" +
-            "   <button onclick='{name}Obj.doEncrypt()'>encrypt</button>" +
-            "   <button onclick='{name}Obj.doDecrypt()'>decrypt</button>" +
-            "</div>").
-            replace(/\{name\}/g, this.name));
+        $("#main").append(generateDiv(this.name))
     };
 
     this.updateText = function (text) {
